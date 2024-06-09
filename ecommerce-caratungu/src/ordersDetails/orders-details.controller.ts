cee460b556b939febf824c7e613bdf27b77b56f9
CTunjano <caratungu@gmail.com>
2024-06-09 17:43:21 -0500

    FIX Guards en POST /orders-details/:id

--- ecommerce-caratungu/src/ordersDetails/orders-details.controller.ts
+++ ecommerce-caratungu/src/ordersDetails/orders-details.controller.ts
@@ -1,20 +1,29 @@
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
 import { OrdersDetailsService } from './orders-details.service';
 import { OrderDetailDto } from './dtos/orders-details.dto';
-import { ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/roles.enum';
 
 @ApiTags('Orders Details')
 @Controller('orders-details')
 export class OrdersDetailsController {
   constructor(private readonly ordersDetailsService: OrdersDetailsService) {}
 
-  @Get()
+  @ApiBearerAuth()
+  @Get(':id')
+  @UseGuards(AuthGuard)
   getOrderDetailsById(@Param('id', ParseUUIDPipe) id: string) {
     return this.ordersDetailsService.getOrderDetailsById(id);
   }
 
   @ApiExcludeEndpoint()
+  @ApiBearerAuth()
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
   createOrderDetails(@Body() orderDetails: OrderDetailDto) {
     return this.ordersDetailsService.createOrderDetails(orderDetails);
   }
